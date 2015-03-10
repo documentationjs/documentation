87a6153516896f9db4a35a962629b6e43a5a90ed
John Firebaugh <john.firebaugh@gmail.com>
2015-03-10 16:53:50 -0700

    2 space indent

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 var test = require('tape'),
-    documentation = require('../'),
-    path = require('path'),
-    concat = require('concat-stream');
+  documentation = require('../'),
+  path = require('path'),
+  concat = require('concat-stream');
 
-test('documentation', function(t) {
-    documentation(path.join(__dirname, 'fixture/simple.js')).pipe(concat(function(data) {
-        t.equal(data.length, 1, 'simple has no dependencies');
-        t.end();
-    }));
+test('documentation', function (t) {
+  documentation(path.join(__dirname, 'fixture/simple.js')).pipe(concat(function (data) {
+    t.equal(data.length, 1, 'simple has no dependencies');
+    t.end();
+  }));
 });
