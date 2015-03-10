b59b111aa9bd9e87955d1b4906549a81a9c0b8a8
Tom MacWright <tom@macwright.org>
2015-03-10 19:55:17 -0400

    4-space, editor config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var fs = require('fs'),
-  doctrine = require('doctrine'),
-  esprima = require('esprima'),
-  through = require('through'),
-  types = require('ast-types'),
-  mdeps = require('module-deps');
+    doctrine = require('doctrine'),
+    esprima = require('esprima'),
+    through = require('through'),
+    types = require('ast-types'),
+    mdeps = require('module-deps');
 
 /**
  * Generate JavaScript documentation as a list of parsed JSDoc
@@ -13,34 +13,34 @@ var fs = require('fs'),
  * @return {Object} stream of output
  */
 module.exports = function(index) {
-  var md = mdeps();
-  md.end({ file: index });
+    var md = mdeps();
+    md.end({ file: index });
 
-  /**
-   * Documentation stream parser: this receives a module-dep item,
-   * reads the file, parses the JavaScript, parses the JSDoc, and
-   * emits parsed comments.
-   * @param {Object} data
-   */
-  function docParserStream(data) {
-    var code = fs.readFileSync(data.file),
-        ast = esprima.parse(code, { attachComment: true }),
-        docs = [];
-    types.visit(ast, {
-      visitMemberExpression: function(path) {
-        var node = path.value;
-        if (node.leadingComments) {
-          node.leadingComments.filter(function(c) {
-            return c.type === 'Block';
-          }).map(function(comment) {
-            docs.push(doctrine.parse(comment.value, { unwrap: true }));
-          });
-        }
-        this.traverse(path);
-      }
-    });
-    docs.forEach(this.push);
-  }
+    /**
+     * Documentation stream parser: this receives a module-dep item,
+     * reads the file, parses the JavaScript, parses the JSDoc, and
+     * emits parsed comments.
+     * @param {Object} data
+     */
+    function docParserStream(data) {
+        var code = fs.readFileSync(data.file),
+            ast = esprima.parse(code, { attachComment: true }),
+            docs = [];
+        types.visit(ast, {
+            visitMemberExpression: function(path) {
+                var node = path.value;
+                if (node.leadingComments) {
+                    node.leadingComments.filter(function(c) {
+                        return c.type === 'Block';
+                    }).map(function(comment) {
+                        docs.push(doctrine.parse(comment.value, { unwrap: true }));
+                    });
+                }
+                this.traverse(path);
+            }
+        });
+        docs.forEach(this.push);
+    }
 
-  return md.pipe(through(docParserStream));
+    return md.pipe(through(docParserStream));
 };
