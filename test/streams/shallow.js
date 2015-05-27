d3fcd4ae9e57d6e8ea490ab31d3fa8542d05a61a
Tom MacWright <tom@macwright.org>
2015-05-27 11:35:28 -0400

    Add tests
    * Add shallow test case
    * Add polyglot test case

---
+++ test/streams/shallow.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var test = require('prova'),
+  concat = require('concat-stream'),
+  path = require('path'),
+  shallow = require('../../streams/shallow');
+
+test('shallow deps', function (t) {
+  shallow([path.resolve(path.join(__dirname, '../fixture/es6.input.js'))])
+    .pipe(concat(function (deps) {
+      t.equal(deps.length, 1);
+      t.ok(deps[0].file, 'has file');
+      t.end();
+    }));
+});
+
+test('shallow deps multi', function (t) {
+  shallow([
+    path.resolve(path.join(__dirname, '../fixture/es6.input.js')),
+    path.resolve(path.join(__dirname, '../fixture/es6.output.json'))
+  ]).pipe(concat(function (deps) {
+    t.equal(deps.length, 2);
+    t.ok(deps[0].file, 'has file');
+    t.end();
+  }));
+});
