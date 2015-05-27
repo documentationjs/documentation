b4efeb84e20c0c81a678d70e1aa69b2a10126093
Beau Gunderson <beau@beaugunderson.com>
2015-05-26 17:12:01 -0700

    Fix github stream

--- streams/github.js
+++ streams/github.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var through = require('through');
+var through2 = require('through2');
 var exec = require('child_process').exec;
 var path = require('path');
 var fs = require('fs');
@@ -47,16 +47,14 @@ function makeGetBase() {
  */
 module.exports = function () {
   var getBase = makeGetBase();
-  return through(function (comment) {
-    this.pause();
+  return through2.obj(function (comment, enc, callback) {
     getBase(comment.context.file, function (base, root) {
       comment.context.path = comment.context.file.replace(root + '/', '');
       comment.context.github = base +
         comment.context.file.replace(root + '/', '') +
         '#L' + comment.context.loc.start.line + '-' +
         'L' + comment.context.loc.end.line;
-      this.push(comment);
-      this.resume();
-    }.bind(this));
+      callback(null, comment);
+    });
   });
 };
