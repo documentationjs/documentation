9a03c7fb0e5a67da501d77d0691ba7944c336fa7
Tom MacWright <tom@macwright.org>
2015-07-10 12:03:39 -0400

    Start to refactor and document helpers for templates

---
+++ docs/markdown.md
@@ -0,0 +1,14 @@
+# Markdown
+
+Markdown templates, just like HTML templates, are [Handlebars](http://handlebarsjs.com/)
+files.
+
+# Helpers
+
+`{{format_params}}`
+
+`{{format_type}}`
+
+`{{format_description}}`
+
+`{{inlines}}`
--- streams/output/html.js
+++ streams/output/html.js
@@ -1,20 +1,17 @@
 'use strict';
 
+var fs = require('fs');
 var through = require('through'),
   File = require('vinyl'),
   vfs = require('vinyl-fs'),
-  slugg = require('slugg'),
-  Remarkable = require('remarkable'),
-  getGlobalExternalLink = require('globals-docs').getDoc,
   path = require('path'),
   Handlebars = require('handlebars'),
   extend = require('extend'),
+  slugg = require('slugg'),
   splicer = require('stream-splicer'),
   hierarchy = require('../hierarchy'),
-  highlight = require('../highlight'),
-  inlineLex = require('jsdoc-inline-lex');
-
-var fs = require('fs');
+  helpers = require('./lib/html_helpers'),
+  highlight = require('../highlight');
 
 /**
  * Make slugg a unary so we can use it in functions
@@ -25,41 +22,6 @@ var fs = require('fs');
  */
 function slug(p) {
   return p ? slugg(p) : '';
-}
-
-/**
- * Format link & tutorial tags with simple code inline tags.
- *
- * @param {string} text input - typically a description
- * @returns {string} markdown-friendly output
- * @private
- * @example
- * formatInlineTags('{@link Foo}'); // "[Foo](#foo)"
- */
-function formatInlineTags(text) {
-  var output = '';
-  var tokens = inlineLex(text);
-
-  function markdownLink(description, href) {
-    return '[`' + description + '`](' + href + ')';
-  }
-
-  for (var i = 0; i < tokens.length; i++) {
-    if (tokens[i].type === 'text') {
-      output += tokens[i].capture[0];
-    } else if (tokens[i].type === 'link') {
-      var parts = tokens[i].capture[1].split(/\s|\|/);
-      if (parts.length === 1) {
-        output += markdownLink(tokens[i].capture[1], slug(tokens[i].capture[1]));
-      } else {
-        output += markdownLink(parts.slice(1).join(' '), slug(parts[0]));
-      }
-    } else if (tokens[i].type === 'prefixLink') {
-      output += markdownLink(tokens[i].capture[1], slug(tokens[i].capture[2]));
-    }
-  }
-
-  return output;
 }
 
 /**
@@ -76,55 +38,9 @@ function formatInlineTags(text) {
  */
 module.exports = function (opts) {
 
-  var md = new Remarkable();
-
   var options = extend({}, {
     path: path.resolve(path.join(__dirname, '../../share/html/'))
   }, opts);
-
-  /**
-   * @name formatMarkdown
-   *
-   * This helper is exposed in templates as `md` and is useful for showing
-   * Markdown-formatted text as proper HTML.
-   * @param {String} string
-   * @returns {String} string
-   * @example
-   * var x = '## foo';
-   * // in template
-   * // {{ md x }}
-   * // generates <h2>foo</h2>
-   */
-  Handlebars.registerHelper('md', function formatMarkdown(string) {
-    return new Handlebars.SafeString(md.render(formatInlineTags(string)));
-  });
-
-  /**
-   * Format a parameter name. This is used in formatParameters
-   * and just needs to be careful about differentiating optional
-   * parameters
-   *
-   * @param {Object} param
-   * @returns {String} formatted parameter representation.
-   */
-  function formatParameter(param) {
-    return (param.type && param.type.type === 'OptionalType') ?
-      '[' + param.name + ']' : param.name;
-  }
-
-  /**
-   * Format the parameters of a function into a quickly-readable
-   * summary that resembles how you would call the function
-   * initially.
-   */
-  function formatParameters() {
-    if (!this.params) return '';
-    return '(' + this.params.map(function (param) {
-      return formatParameter(param);
-    }).join(', ') + ')';
-  }
-
-  Handlebars.registerHelper('format_params', formatParameters);
 
   var pageTemplate = Handlebars
     .compile(fs.readFileSync(path.join(options.path, 'index.hbs'), 'utf8'));
@@ -136,67 +52,13 @@ module.exports = function (opts) {
 
   var htmlStream = through(function (comments) {
 
-    /**
-     * @name formatType
-     *
-     * Helper used to format JSDoc-style type definitions into HTML.
-     * @param {Object} type
-     * @returns {String} string
-     * @example
-     * var x = { type: 'NameExpression', name: 'String' };
-     * // in template
-     * // {{ type x }}
-     * // generates String
-     */
-    function formatType(type, html) {
-      switch (type.type) {
-        case 'NameExpression':
-          return html ? '<code>' + autolink(type.name) + '</code>' : type.name;
-        case 'UnionType':
-          return type.elements.map(function (element) {
-            return formatType(element, html);
-          }).join(' or ');
-        case 'AllLiteral':
-          return 'Any';
-        case 'OptionalType':
-          return '<code>[' + formatType(type.expression, html) + ']</code>';
-        case 'TypeApplication':
-          return formatType(type.expression) + '<' +
-            type.applications.map(function (application) {
-              return formatType(application, html);
-            }).join(', ') + '>';
-      }
-    }
-
     var paths = comments.map(function (comment) {
       return comment.path.map(slug).join('/');
     }).filter(function (path) {
       return path;
     });
 
-    Handlebars.registerHelper('format_type', function (type) {
-      return formatType(type, true);
-    });
-
-    Handlebars.registerHelper('permalink', function () {
-      return this.path.map(slug).join('/');
-    });
-
-    /**
-     * Link text to this page or to a central resource.
-     * @param {string} text
-     * @returns {string} potentially linked HTML
-     */
-    function autolink(text) {
-      if (paths.indexOf(slug(text)) !== -1) {
-        return '<a href="#' + slug(text) + '">' + text + '</a>';
-      } else if (getGlobalExternalLink(text)) {
-        return '<a href="' + getGlobalExternalLink(text) + '">' + text + '</a>';
-      }
-      return text;
-    }
-
-    Handlebars.registerHelper('autolink', autolink);
+    helpers(Handlebars, paths);
 
     this.push(new File({
       path: 'index.json',
