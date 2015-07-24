27bbe6310a3ba3d18b37391da723be78002abc2e
Tom MacWright <tom@macwright.org>
2015-07-24 15:40:03 -0400

    Better docs for shallow stream. Fixes #117

--- streams/input/shallow.js
+++ streams/input/shallow.js
@@ -7,8 +7,13 @@ var fs = require('fs');
  * A readable source for content that doesn't do dependency resolution, but
  * simply reads files and pushes them onto a stream.
  *
- * This stream requires filesystem access, and thus isn't suitable
- * for a browser environment.
+ * If an array of strings is provided as input to this method, then
+ * they will be treated as filenames and read into the stream.
+ *
+ * If an array of objects is provided, then we assume that they are valid
+ * objects with `source` and `file` properties, and don't use the filesystem
+ * at all. This is one way of getting documentation.js to run in a browser
+ * or without fs access.
  *
  * @param {Array<string|Object>} indexes entry points
  * @return {ReadableStream} this emits data
