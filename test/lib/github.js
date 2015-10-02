bcb9e811859f2eb124bb687238626f89f57792ef
Tom MacWright <tom@macwright.org>
2015-10-01 21:44:00 -0400

    Add tests for github processor

--- test/lib/github.js
+++ test/lib/github.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var test = require('tap').test,
+  mock = require('mock-fs'),
+  mockRepo = require('./git/mock_repo'),
   parse = require('../../lib/parsers/javascript'),
   github = require('../../lib/github');
 
@@ -8,13 +10,30 @@ function toComment(fn, filename) {
   return parse({
     file: filename,
     source: fn instanceof Function ? '(' + fn.toString() + ')' : fn
-  })[0];
+  }).map(github);
 }
 
 function evaluate(fn) {
-  return toComment(fn, 'input.js');
+  return toComment(fn, '/my/repository/path/index.js');
 }
 
 test('github', function (t) {
+
+  mock(mockRepo);
+
+  t.equal(evaluate(function () {
+    /**
+     * get one
+     * @returns {number} one
+     */
+    function getOne() {
+      return 1;
+    }
+  })[0].context.github,
+  'https://github.com/foo/bar/blob/this_is_the_sha/index.js#L6-L8',
+  'gets github url');
+
+  mock.restore();
+
   t.end();
 });
