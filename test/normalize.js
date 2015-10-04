6c4f1f68b4ee70e0951afa1ec1e55f9269e63d98
Tom MacWright <tom@macwright.org>
2015-10-04 12:12:57 -0400

    Clean up github test

--- test/normalize.js
+++ test/normalize.js
@@ -1,6 +1,9 @@
 function normalize(comments) {
   comments.forEach(function (comment) {
     delete comment.context.file;
+    if (comment.context.github) {
+      comment.context.github = '[github]';
+    }
     normalize(comment.members.instance);
     normalize(comment.members.static);
   });
