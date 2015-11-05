8a2ec3efad6c753607dda9ebe587b94239d5783d
Tom MacWright <tom@macwright.org>
2015-11-05 14:34:57 -0500

    Allow param tag type to be inferred and merged with description

---
+++ test/fixture/merge-infered-type.input.js
@@ -0,0 +1,9 @@
+/**
+ * Add five to `x`.
+ *
+ * @param x The number to add five to.
+ * @returns {number} x plus five.
+ */
+function addFive(x: number): number {
+  return x + 5;
+}
---
+++ test/fixture/merge-infered-type.output.custom.md
@@ -0,0 +1,9 @@
+# addFive
+
+Add five to `x`.
+
+**Parameters**
+
+-   `x` **number** The number to add five to.
+
+Returns **number** x plus five.
