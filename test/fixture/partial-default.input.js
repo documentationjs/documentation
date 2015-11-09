33552c6e8a8b9308e066dd871911691578567dc1
Tom MacWright <tom@macwright.org>
2015-11-09 12:51:15 -0500

    Fix type merging. Fixes #233
    Some params cannot be identified with only param.name. This
    changes the algorithm to transform parameters into doc objects
    first, and then using the value of doc.name, which is defined
    even for parameters with default values.

---
+++ test/fixture/partial-default.input.js
@@ -0,0 +1,8 @@
+/**
+ * Number
+ *
+ * @param {number} x an argument
+ *
+ * @returns {number} some
+ */
+export const myfunc = (x = 123) => x;
---
+++ test/fixture/partial-default.output.custom.md
@@ -0,0 +1,9 @@
+# myfunc
+
+Number
+
+**Parameters**
+
+-   `x` **number** an argument
+
+Returns **number** some
