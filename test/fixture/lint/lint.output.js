420829356521268b7edee7de02b8af59fc9cc063
Tom MacWright <tom@macwright.org>
2015-11-24 11:41:42 -0500

    Fix test fixture

--- test/fixture/lint/lint.output.js
+++ test/fixture/lint/lint.output.js
@@ -1,5 +1,4 @@
         1:1  warning  could not determine @name for hierarchy
-        1:1  warning  could not determine @name for hierarchy
         2:1  warning  type String found, string is standard
         3:1  warning  type Number found, number is standard
         4:1  warning  type Number found, number is standard
