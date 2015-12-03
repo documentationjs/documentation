4acd658617928bd17ae7364ef2512630d97c007a
Tom MacWright <tom@macwright.org>
2015-12-03 17:04:58 -0500

    Fix circular references, fix inferencers

--- test/normalize.js
+++ test/normalize.js
@@ -1,7 +1,9 @@
+var removeProperties = require('babel-traverse').default.removeProperties;
 var walk = require('../lib/walk');
 
 module.exports = function (comments) {
   return walk(comments, function (comment) {
+    comment.context.ast = removeProperties(comment.context.ast);
     delete comment.context.file;
     if (comment.context.github) {
       comment.context.github = '[github]';
