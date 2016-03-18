5a5ebaf7630202d6c781330f398e62c8dc254adf
Tom MacWright <tom@macwright.org>
2016-03-18 17:11:11 -0400

    Unkink option parsing options

--- bin/documentation.js
+++ bin/documentation.js
@@ -13,57 +13,15 @@ var parsedArgs = parseArgs();
 commands[parsedArgs.command](documentation, parsedArgs);
 
 function parseArgs() {
-  // reset() needs to be called at parse time because the yargs module uses an
-  // internal global variable to hold option state
-  var argv = addCommands(yargs, true)
-    .usage('Usage: $0 <command> [options]')
+
+  var commandArgv = addCommands(yargs)
     .version(function () {
       return require('../package').version;
     })
-    .option('shallow', {
-      describe: 'shallow mode turns off dependency resolution, ' +
-      'only processing the specified files (or the main script specified in package.json)',
-      default: false,
-      type: 'boolean'
-    })
-    .option('config', {
-      describe: 'configuration file. an array defining explicit sort order',
-      alias: 'c'
-    })
-    .option('external', {
-      describe: 'a string / glob match pattern that defines which external ' +
-        'modules will be whitelisted and included in the generated documentation.',
-      default: null
-    })
-    .option('extension', {
-      describe: 'only input source files matching this extension will be parsed, ' +
-        'this option can be used multiple times.',
-      alias: 'e'
-    })
-    .option('polyglot', {
-      type: 'boolean',
-      describe: 'polyglot mode turns off dependency resolution and ' +
-        'enables multi-language support. use this to document c++'
-    })
-    .option('private', {
-      describe: 'generate documentation tagged as private',
-      type: 'boolean',
-      default: false,
-      alias: 'p'
-    })
-    .option('access', {
-      describe: 'Include only comments with a given access level, out of private, ' +
-        'protected, public, undefined. By default, public, protected, and undefined access ' +
-        'levels are included',
-      choices: ['public', 'private', 'protected', 'undefined'],
-      alias: 'a'
-    })
-    .option('github', {
-      type: 'boolean',
-      describe: 'infer links to github in documentation',
-      alias: 'g'
-    })
-  .argv;
+    .argv;
+
+  var argv = commands[commandArgv._[0]]
+    .parseArgs(yargs.reset()).argv;
 
   var options = {};
   if (argv.config) {
@@ -109,14 +67,8 @@ function parseArgs() {
 }
 
 function addCommands(parser, descriptionOnly) {
-  parser = parser.demand(1);
-  for (var cmd in commands) {
-    if (descriptionOnly) {
-      parser = parser.command(cmd, commands[cmd].description);
-    } else {
-      parser = parser.command(cmd, commands[cmd].description, commands[cmd].parseArgs);
-    }
-  }
-  return parser.help('help');
+  return Object.keys(commands).reduce(function(parser, cmd) {
+    return parser.command(cmd, commands[cmd].description);
+  }, parser.demand(1)).help('help');
 }
 
