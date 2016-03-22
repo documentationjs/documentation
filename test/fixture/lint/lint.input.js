2d84ab296f902f10e98459e787f7a6cade53b393
Tom MacWright <tom@macwright.org>
2016-03-22 18:54:23 -0400

    Check for default tags.

--- test/fixture/lint/lint.input.js
+++ test/fixture/lint/lint.input.js
@@ -4,9 +4,13 @@
  * @param {Array<Number>} foo bar
  * @param {Array|Number} foo bar
  * @memberof notfound
+ * @abstract
+ * @abstract
  */
 
 /**
  * @param {String} baz bar
  * @private
+ * @private
+ * @access private
  */
