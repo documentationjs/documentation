6ab7ef5b4398a6be33b7d091917a80215dd7cc26
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-05-25 17:06:18 -0700

    Pick up name and parameters from exported functions
    This makes sure that we get the name and params from exported
    functions/classes/vars.
    
    For export default the name is inferred from the file name.
    
    Towards #424

---
+++ test/lib/infer/params.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var test = require('tap').test,
+  parse = require('../../../lib/parsers/javascript'),
+  inferParams = require('../../../lib/infer/params')();
+
+function toComment(fn, file) {
+  return parse({
+    file: file,
+    source: fn instanceof Function ? '(' + fn.toString() + ')' : fn
+  })[0];
+}
+
+function evaluate(fn, file) {
+  return inferParams(toComment(fn, file));
+}
+
+test('inferParams', function (t) {
+  t.deepEqual(evaluate(function () {
+    /** Test */
+    function f(x) {}
+  }).params, [{lineNumber: 3, name: 'x', title: 'param'}]);
+
+  t.deepEqual(evaluate(`
+    /** Test */
+    export function f(x) {}
+  `).params, [{lineNumber: 3, name: 'x', title: 'param'}]);
+
+  t.deepEqual(evaluate(`
+    /** Test */
+    export default function f(x) {}
+  `).params, [{lineNumber: 3, name: 'x', title: 'param'}]);
+
+  t.end();
+});
