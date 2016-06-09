7352ab4fbaf08276d92a29f0d06919fa80a6084a
Oscar Dub <oscardub@gmail.com>
2016-06-09 13:55:06 +0100

    Fix filter functionality.

--- default_theme/assets/site.js
+++ default_theme/assets/site.js
@@ -11,7 +11,7 @@ var tocElements = document.getElementById('toc')
 document.getElementById('filter-input')
   .addEventListener('keyup', function (e) {
 
-    var i, element;
+    var i, element, children;
 
     // enter key
     if (e.keyCode === 13) {
@@ -32,14 +32,15 @@ document.getElementById('filter-input')
     var value = this.value.toLowerCase();
 
     if (!value.match(/^\s*$/)) {
-      match = function (text) {
-        return text.toLowerCase().indexOf(value) !== -1;
+      match = function (element) {
+        return element.firstChild.innerHTML.toLowerCase().indexOf(value) !== -1;
       };
     }
 
     for (i = 0; i < tocElements.length; i++) {
       element = tocElements[i];
-      if (match(element.firstChild.innerHTML)) {
+      children = Array.from(element.getElementsByTagName('li'));
+      if (match(element) || children.some(match)) {
         element.classList.remove('display-none');
       } else {
         element.classList.add('display-none');
