a4889c97ecbd6a2b11442ef2abd06c7b7459fbd2
Tom MacWright <tom@macwright.org>
2016-06-16 13:58:11 -0400

    [wip] Test support for namepaths containing special characters

--- test/fixture/class.input.js
+++ test/fixture/class.input.js
@@ -1,23 +1,37 @@
-/**
- * This is my class, a demo thing.
- * @class MyClass
- * @property {number} howMany how many things it contains
- */
-function MyClass() {
-  this.howMany = 2;
-}
+// /**
+//  * This is my class, a demo thing.
+//  * @class MyClass
+//  * @property {number} howMany how many things it contains
+//  */
+// function MyClass() {
+//   this.howMany = 2;
+// }
+// 
+// /**
+//  * Get the number 42
+//  * @param {boolean} getIt whether to get the number
+//  * @returns {number} forty-two
+//  */
+// MyClass.prototype.getFoo = function (getIt) {
+//   return getIt ? 42 : 0;
+// };
+// 
+// /**
+//  * Get undefined
+//  * @returns {undefined} does not return anything.
+//  */
+// MyClass.prototype.getUndefined = function () { };
+// 
+// /**
+//  * A colon-separated name
+//  * @returns {undefined} does not return anything.
+//  */
+// MyClass.prototype['colon:name'] = function () { };
 
 /**
- * Get the number 42
- * @param {boolean} getIt whether to get the number
- * @returns {number} forty-two
- */
-MyClass.prototype.getFoo = function (getIt) {
-  return getIt ? 42 : 0;
-};
-
-/**
- * Get undefined
+ * A colon-separated name specified manually
+ *
+ * @name "colon:foo"
  * @returns {undefined} does not return anything.
  */
-MyClass.prototype.getUndefined = function () { };
+MyClass.prototype['colon:foo'] = function () { };
