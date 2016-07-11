b4fe8edf92af35315d6f477311e2c9710e0056e3
Anand Thakker <anandthakker@users.noreply.github.com>
2016-07-11 15:03:41 -0400

    Fixes TOC entries missing description in `serve` (#470)
    * Fixes TOC entries missing description in `serve`
    
    * Remove console statements

--- test/lib/sort.js
+++ test/lib/sort.js
@@ -108,3 +108,72 @@ test('sort stream with configuration and a section', function (t) {
 
   t.end();
 });
+
+test('sort an already-sorted stream containing a section/description', function (t) {
+  // this happens in the 'serve' task
+  var apples = { context: { sortKey: 'a' }, name: 'apples' };
+  var carrot = { context: { sortKey: 'b' }, name: 'carrot' };
+  var bananas = { context: { sortKey: 'c' }, name: 'bananas' };
+
+  var section = { name: 'This is the banana type', description: 'here lies bananas' };
+  var sectionMarkdown = {
+    'name': 'This is the banana type',
+    'description': {
+      'type': 'root',
+      'children': [{
+        'type': 'paragraph',
+        'children': [{
+          'type': 'text',
+          'value': 'here lies bananas',
+          'position': {
+            'start': {
+              'line': 1,
+              'column': 1,
+              'offset': 0
+            },
+            'end': {
+              'line': 1,
+              'column': 18,
+              'offset': 17
+            },
+            'indent': []
+          }
+        }],
+        'position': {
+          'start': {
+            'line': 1,
+            'column': 1,
+            'offset': 0
+          },
+          'end': {
+            'line': 1,
+            'column': 18,
+            'offset': 17
+          },'indent': []
+        }
+      }],
+      'position': {
+        'start': {
+          'line': 1,
+          'column': 1,
+          'offset': 0
+        },
+        'end': {
+          'line': 1,
+          'column': 18,
+          'offset': 17
+        }
+      }
+    },
+    'kind': 'note'
+  };
+
+  var config = {
+    toc: ['carrot', section, 'bananas']
+  };
+
+  var sortOnce = sort([apples, carrot, bananas], config);
+  var sortTwice = sort(sortOnce, config);
+  t.deepEqual(sortTwice, [carrot, sectionMarkdown, bananas, apples]);
+  t.end();
+});
