2a86d7f21dc4033658f66099e92feb62e3c53131
Tom MacWright <tom@macwright.org>
2016-07-16 20:23:06 -0400

    Lint all type-containing tags. Fixes #380

--- test/fixture/lint/lint.input.js
+++ test/fixture/lint/lint.input.js
@@ -3,10 +3,13 @@
  * @param {Array<Number>} foo bar
  * @param {Array<Number>} foo bar
  * @param {Array|Number} foo bar
+ * @returns {object} bad object return type
+ * @type {Array<object>} bad object type
  * @memberof notfound
  */
 
 /**
  * @param {String} baz bar
+ * @property {String} bad property
  * @private
  */
--- test/fixture/lint/lint.output.js
+++ test/fixture/lint/lint.output.js
@@ -2,8 +2,11 @@
         3:1  warning  type Number found, number is standard
         4:1  warning  type Number found, number is standard
         5:1  warning  type Number found, number is standard
-        6:1  warning  @memberof reference to notfound not found
-        9:1  warning  could not determine @name for hierarchy
-       10:1  warning  type String found, string is standard
+        6:1  warning  type object found, Object is standard
+        7:1  warning  type object found, Object is standard
+        8:1  warning  @memberof reference to notfound not found
+       11:1  warning  could not determine @name for hierarchy
+       12:1  warning  type String found, string is standard
+       13:1  warning  type String found, string is standard
 
-⚠ 8 warnings
+⚠ 11 warnings
