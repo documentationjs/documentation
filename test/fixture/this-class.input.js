6855e07ae743fc9e840c9300ff32e152aa76fc21
Tom MacWright <tom@macwright.org>
2016-11-15 11:07:35 -0500

    Fix membership assignment for old-fashioned prototype members
    Fixes #598
    
    This _changes behavior_: previously in the case of
    
    ```js
    /** base */
    var Foo = function Bar() {
      {
        /** */
        this.baz = 0;
      }
    };
    ```
    
    We assigned baz to Bar. Now we will assign Baz to Foo. I believe this is
    right, since in JavaScript Bar is not actually bound in this code.

---
+++ test/fixture/this-class.input.js
@@ -0,0 +1,13 @@
+/** @module bookshelf */
+
+/** @class */
+var Book = function(title) {
+    /** The title of the book. */
+   this.title = title;
+}
+
+/** @class */
+this.BookShelf = function(title) {
+    /** The title of the bookshelf. */
+   this.title = title;
+}
