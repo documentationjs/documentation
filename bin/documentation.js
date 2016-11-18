5193cb4072dea5f649a2cc8dc723163717c50829
Tom MacWright <tom@macwright.org>
2016-11-18 13:21:01 -0500

    Add more usage examples. Fixes #526

--- bin/documentation.js
+++ bin/documentation.js
@@ -15,6 +15,28 @@ var argv = yargs
   .version(function () {
     return require('../package').version;
   })
+  .usage('Usage:\n\n' +
+    '# generate markdown docs for index.js and files it references\n' +
+    '$0 build index.js -f md\n\n' +
+
+    '# generate html docs for all files in src\n' +
+    '$0 build src/** -f html -o docs\n\n' +
+
+    '# document index.js, ignoring any files it requires or imports\n' +
+    '$0 build index.js -f md --shallow\n\n' +
+
+    '# build, serve, and live-update html docs for app.js\n' +
+    '$0 serve app.js\n\n' +
+
+    '# validate JSDoc syntax in util.js\n' +
+    '$0 lint util.js\n\n' +
+
+    '# update the API section of README.md with docs from index.js\n' +
+    '$0 readme index.js --section=API\n\n' +
+
+    '# build docs for all values exported by index.js\n' +
+    '$0 build --document-exported index.js'
+  )
   .recommendCommands()
   .help()
   .argv;
