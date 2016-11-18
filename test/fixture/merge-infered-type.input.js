29cd58bb3165e6791740015f4634ecd7cbdf55d5
Tom MacWright <tmcw@users.noreply.github.com>
2016-11-18 11:59:15 -0500

    Merge inferred return type like we do for params. Refs #359 (#604)

--- test/fixture/merge-infered-type.input.js
+++ test/fixture/merge-infered-type.input.js
@@ -2,7 +2,7 @@
  * Add five to `x`.
  *
  * @param x The number to add five to.
- * @returns {number} x plus five.
+ * @returns x plus five.
  */
 function addFive(x: number): number {
   return x + 5;
