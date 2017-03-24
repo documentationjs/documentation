467139e8b25c65603aca1233a9cafbfeeb29dfae
Anton Shchekota <anton.redfox@gmail.com>
2017-03-25 00:04:16 +0300

    Fixed find_git test for windows platform.

--- test/lib/git/find_git.js
+++ test/lib/git/find_git.js
@@ -3,16 +3,19 @@
 var test = require('tap').test,
   mock = require('mock-fs'),
   mockRepo = require('./mock_repo'),
+  path = require('path'),
   findGit = require('../../../lib/git/find_git');
 
 test('findGit', function (t) {
 
   mock(mockRepo.master);
 
+  const root = path.parse(__dirname).root;
+
   t.equal(
     findGit(
-      '/my/repository/path/index.js'),
-      '/my/repository/path/.git', 'finds git path');
+      root + path.join('my', 'repository', 'path', 'index.js')),
+      root + path.join('my', 'repository', 'path', '.git'), 'finds git path');
 
   mock.restore();
 
