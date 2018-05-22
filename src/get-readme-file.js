ae789deac79d359819e1ace6f6dc967183674c81
Tom MacWright <tom@macwright.org>
2018-05-21 20:39:00 -0700

    fix: Don't detect non-markdown readme types, add tests

--- src/get-readme-file.js
+++ src/get-readme-file.js
@@ -5,14 +5,11 @@ const path = require('path');
 
 module.exports = function findReadme(dir: string) {
   const readmeFilenames = [
-    'README',
     'README.markdown',
     'README.md',
-    'README.txt',
     'Readme.md',
     'readme.markdown',
-    'readme.md',
-    'readme.txt'
+    'readme.md'
   ];
 
   const readmeFile = fs.readdirSync(dir).find(function(filename) {
