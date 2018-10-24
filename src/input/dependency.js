c85a83f726f6b96311e68b58e232fc395c0058b5
Hugo Dias <hugomrdias@gmail.com>
2018-10-24 10:50:04 +0100

    feat: add support for node resolve algo

--- src/input/dependency.js
+++ src/input/dependency.js
@@ -71,11 +71,13 @@ function dependencyStream(
       })
     ],
     postFilter: moduleFilters.externals(indexes, config),
-    resolve: (id, opts, cb) => {
-      const r = require('resolve');
-      opts.basedir = path.dirname(opts.filename);
-      r(id, opts, cb);
-    }
+    resolve:
+      config.resolve === 'node' &&
+      ((id, opts, cb) => {
+        const r = require('resolve');
+        opts.basedir = path.dirname(opts.filename);
+        r(id, opts, cb);
+      })
   });
   smartGlob(indexes, config.parseExtension).forEach(index => {
     md.write(path.resolve(index));
