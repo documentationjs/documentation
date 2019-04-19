1e93fa1e6d723bff8888d9b349b205aa0340b3f1
Devon Govett <devongovett@gmail.com>
2019-04-18 20:33:08 -0700

    Infer param types from typescript annotations

--- __tests__/lib/infer/params.js
+++ __tests__/lib/infer/params.js
@@ -203,3 +203,46 @@ test('inferParams', function() {
     ).params
   ).toMatchSnapshot();
 });
+
+test('inferParams (typescript)', function() {
+  expect(
+    evaluate(`/** Test */function f(a: string) {};`, 'test.ts').params
+  ).toMatchSnapshot();
+
+  expect(
+    evaluate(`/** Test */function f([a: string, b, {c}]) {};`, 'test.ts').params
+  ).toMatchSnapshot();
+
+  expect(
+    evaluate(
+      `
+    /** Test
+     * @param x
+    */
+    function f(x: number = 4) {}
+  `
+  , 'test.ts').params
+  ).toMatchSnapshot();
+
+  expect(
+    evaluate(
+      `
+    /** Test */
+    function f(opts: { x: string }) {}
+  ` , 'test.ts'
+    ).params
+  ).toMatchSnapshot();
+
+  expect(
+    evaluate(
+      `
+    /** Test */
+    function f(opts: { [foo]: string }) {}
+  ` , 'test.ts'
+    ).params
+  ).toMatchSnapshot();
+
+  expect(
+    evaluate(`/** Test */function f(...a: string) {};`, 'test.ts').params
+  ).toMatchSnapshot();
+});
