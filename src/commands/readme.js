226a666d1e1f465e357d21fdd36735dfdec55fba
Ben Turner <7623873+ben-turner@users.noreply.github.com>
2019-07-16 14:37:41 -0700

    deps: Remove unmaintained dependency: disparity (#1264)
    * Remove unmaintained dependency: disparity
    
    The dependency disparity has not been updated for 4 years and is using
    a very old version of the diff package which has a security alert.
    
    This alert is not relevant to the current usage of the package, however
    it does generate noise when installing. Additionally, it seems unwise to
    be relying on unmaintained packages.
    
    This commit replaces disparity with diff and adds some code to color the
    output which is based on disparity's logic.
    
    * Use chalk to color diff output instead of ansi-styles

--- src/commands/readme.js
+++ src/commands/readme.js
@@ -5,7 +5,7 @@ const documentation = require('../');
 const sharedOptions = require('./shared_options');
 const inject = require('mdast-util-inject');
 const chalk = require('chalk');
-const disparity = require('disparity');
+const diff = require('diff');
 const getReadmeFile = require('../get-readme-file');
 
 module.exports.command = 'readme [input..]';
@@ -94,22 +94,48 @@ module.exports.handler = function readme(argv) {
         .process(readmeContent)
     )
     .then(file => {
-      const diffOutput = disparity.unified(readmeContent, file.contents, {
-        paths: [argv.readmeFile, argv.readmeFile]
-      });
-      if (!diffOutput.length) {
+      const diffRaw = diff.createPatch(
+        '',
+        readmeContent,
+        file.contents,
+        '',
+        ''
+      );
+      if (!diffRaw.length) {
         log(`${argv.readmeFile} is up to date.`);
         process.exit(0);
       }
 
+      // Replace diff headers with real values
+      const cleanedDiff = diffRaw
+        .replace(/^([^\n]+)\n([^\n]+)\n/m, '')
+        .replace(/^---.*/gm, `--- ${argv.readmeFile}\tremoved`)
+        .replace(/^\+\+\+.*/gm, `+++ ${argv.readmeFile}\tadded`);
+
+      // Includes newlines for easier joins
+      const diffLines = cleanedDiff.split(/^/m);
+      const diffHeader = diffLines
+        .slice(0, 2)
+        .join('')
+        .replace(/[^\n\r]+/g, chalk.yellow('$&'));
+      const diffBody = diffLines
+        .slice(2)
+        .join('')
+        .replace(/^-[^\n\r]*/gm, chalk.red('$&'))
+        .replace(/^\+[^\n\r]*/gm, chalk.green('$&'))
+        .replace(/^@@.+@@/gm, chalk.magenta('$&'));
+
       if (argv.d) {
         log(
           chalk.bold(`${argv.readmeFile} needs the following updates:`),
-          `\n${diffOutput}`
+          `\n${diffHeader}${diffBody}`
         );
         process.exit(1);
       } else {
-        log(chalk.bold(`Updating ${argv.readmeFile}`), `\n${diffOutput}`);
+        log(
+          chalk.bold(`Updating ${argv.readmeFile}`),
+          `\n${diffHeader}${diffBody}`
+        );
       }
 
       fs.writeFileSync(argv.readmeFile, file.contents);
