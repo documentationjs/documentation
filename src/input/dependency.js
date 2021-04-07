39097b0d0322f572b6c51637ad08dc483b826e24
Anton Shchekota <anton.redfox@gmail.com>
2021-04-07 22:18:17 +0300

    deps: remove deps which already in stage 4

--- src/input/dependency.js
+++ src/input/dependency.js
@@ -17,19 +17,14 @@ const STANDARD_BABEL_CONFIG = {
     '@babel/plugin-proposal-function-bind',
     // Stage 1
     '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-logical-assignment-operators',
-    '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
     '@babel/plugin-proposal-do-expressions',
     // Stage 2
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
     '@babel/plugin-proposal-function-sent',
     '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-numeric-separator',
     '@babel/plugin-proposal-throw-expressions',
     // Stage 3
-    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
     ['@babel/plugin-proposal-class-properties', { loose: false }],
     '@babel/plugin-proposal-private-methods',
